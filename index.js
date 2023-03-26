51a236c2e5e1d63f85cc8a4d9133048ea2b87aac
Bouba <boubacar.konate@outlook.fr>
2023-03-26 22:33:46 +0200

    hearder à continuer

---
+++ index.js
@@ -0,0 +1,10 @@
+const menuHamburger = document.querySelector(".menuHamburger")
+const navLinks = document.querySelector(".menuLiens")
+
+menuHamburger.addEventListener('click',()=>{
+navLinks.classList.toggle('mobile-menu')
+})
+
+
+
+
